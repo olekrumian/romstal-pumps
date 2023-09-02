b5ba1de7a0f7948156b59ff916660789bf9e1930
Oleksandr Rumiantsev <58187457+olekrumian@users.noreply.github.com>
2023-09-02 23:19:08 +0200

    callForm

--- main.js
+++ main.js
@@ -21,6 +21,18 @@ navToggle.addEventListener('click', () => {
   }
 })
 
+/* Call Form */
+const callFormInput = document.querySelector('.call-form-input')
+
+callFormInput.addEventListener('keypress', (e) => {
+  let newValue = e.target.value
+  if (!newValue.startsWith('+38')) {
+    newValue = `+38${newValue}`
+  }
+  callFormInput.value = newValue
+})
+/* End of Call Form */
+
 // document.addEventListener('DOMContentLoaded', () => {
 //   date.innerHTML = `Romstal Ukraine © ${new Date().getFullYear()}`
 // })
@@ -54,69 +66,5 @@ navToggle.addEventListener('click', () => {
 /* First slider pumps end */
 
 /* Start datalist */
-class DataList {
-  constructor(containerId, inputId, listId, options) {
-    this.containerId = containerId
-    this.inputId = inputId
-    this.listId = listId
-    this.options = options
-  }
-
-  create(filter = '') {
-    const list = document.getElementById(this.listId)
-    const filterOptions = this.options.filter(
-      (d) => filter === '' || d.text.includes(filter)
-    )
-
-    if (filterOptions.length === 0) {
-      list.classList.remove('active')
-    } else {
-      list.classList.add('active')
-    }
-
-    list.innerHTML = filterOptions
-      .map((o) => `<li id=${o.value}>${o.text}</li>`)
-      .join('')
-  }
-
-  addListeners(datalist) {
-    const container = document.getElementById(this.containerId)
-    const input = document.getElementById(this.inputId)
-    const list = document.getElementById(this.listId)
-    container.addEventListener('click', (e) => {
-      if (e.target.id === this.inputId) {
-        container.classList.toggle('active')
-      } else if (e.target.id === 'datalist-icon') {
-        container.classList.toggle('active')
-        input.focus()
-      }
-    })
-
-    input.addEventListener('input', function (e) {
-      if (!container.classList.contains('active')) {
-        container.classList.add('active')
-      }
-
-      datalist.create(input.value)
-    })
-
-    list.addEventListener('click', function (e) {
-      if (e.target.nodeName.toLocaleLowerCase() === 'li') {
-        input.value = e.target.innerText
-        container.classList.remove('active')
-      }
-    })
-  }
-}
-
-const data = [
-  { value: '1', text: 'Низька (5 см)' },
-  { value: '2', text: 'Середня (10 см )' },
-  { value: '3', text: 'Висока (більше 10 см )' },
-]
-
-const datalist = new DataList('datalist', 'datalist-input', 'datalist-ul', data)
-datalist.create()
-datalist.addListeners(datalist)
 
 /* End datalist */
