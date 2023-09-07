e802a0281d7102bb895d7dc1352b5f595e651737
Oleksandr Rumiantsev <58187457+olekrumian@users.noreply.github.com>
2023-09-07 11:16:04 +0200

    tooMuch

--- main.js
+++ main.js
@@ -22,25 +22,69 @@ navToggle.addEventListener('click', () => {
 })
 
 /* Call Form */
-const callFormInput = document.querySelector('.call-form-input')
+const callFormInput = document.querySelectorAll('.call-form-input')
 
-callFormInput.addEventListener('keypress', (e) => {
-  let newValue = e.target.value
-  if (!newValue.startsWith('+38')) {
-    newValue = `+38${newValue}`
-  }
-  callFormInput.value = newValue
+callFormInput.forEach((element) => {
+  element.addEventListener('keypress', (e) => {
+    let newValue = e.target.value
+    if (!newValue.startsWith('+38')) {
+      newValue = `+38${newValue}`
+      e.target.value = newValue
+    }
+  })
 })
 /* End of Call Form */
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   date.innerHTML = `Romstal Ukraine © ${new Date().getFullYear()}`
-// })
+document.addEventListener('DOMContentLoaded', () => {
+  date.innerHTML = `Romstal Ukraine © ${new Date().getFullYear()}`
+})
+
+function scrollToSection(e, offset) {
+  e.preventDefault()
+
+  const targetId = e.target.getAttribute('href')
+  const targetSection = document.querySelector(targetId)
+
+  if (targetSection) {
+    const targetPosition = targetSection.offsetTop + offset
+    window.scrollTo({
+      top: targetPosition,
+    })
+  }
+
+  if (visibility) {
+    navToggle.setAttribute('aria-expanded', 'false')
+    navigation.setAttribute('data-visible', false)
+    navToggle.style.background = `url('./assets/icon/hamburger.svg') no-repeat center`
+    document.body.style.overflow = ''
+  }
+}
+
+function scrollToCall(sectionId, offset) {
+  const section = document.getElementById(sectionId)
+  const topPos =
+    section.getBoundingClientRect().top + window.pageYOffset + offset
+  window.scrollTo({ top: topPos })
+
+  if (visibility) {
+    navToggle.setAttribute('aria-expanded', 'false')
+    navigation.setAttribute('data-visible', false)
+    navToggle.style.background = `url('./assets/icon/hamburger.svg') no-repeat center`
+    document.body.style.overflow = ''
+  }
+}
+navigationLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    scrollToSection(e, -83)
+  })
+})
 
 /* First slider pumps start */
 
 /* First slider pumps end */
 
 /* Start datalist */
-
 /* End datalist */
+
+/* Completed slider */
+/* End of Completed swiper */
